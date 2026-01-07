a6fea5f602f9e855bb0368e6ef82cfa861487e31
Kingsley Chen <kingsleyc6@nycstudents.net>
2026-01-07 18:14:38 +0000

    ftdyrt

--- WDLesson3_3Practical/script.js
+++ WDLesson3_3Practical/script.js
@@ -1,20 +1,20 @@
 /* Challenge 2: Create a function for each image that display a description of the picture into the div with an id of highlight*/
     function background1(){
-        document.getElementById("highlight").innerHTML = "back ground 1"
+        document.getElementById("container gallery").innerHTML = "back ground 1"
     }
 
     function background2(){
-        document.getElementById("highlight").innerHTML = "back ground 2"
+        document.getElementById("container gallery").innerHTML = "back ground 2"
     }
 
     function background3(){
-        document.getElementById("highlight").innerHTML = "back ground 3"
+        document.getElementById("container gallery").innerHTML = "back ground 3"
     }
 
     function background4(){
-        document.getElementById("highlight").innerHTML = "back ground 4"
+        document.getElementById("container gallery").innerHTML = "back ground 4"
     }
 
     function background5(){
-        document.getElementById("highlight").innerHTML = "back ground 5"
+        document.getElementById("container gallery").innerHTML = "back ground 5"
     }
