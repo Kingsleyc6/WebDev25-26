f152889dc974245ec80ce8b4f0c09f66b0628049
Kingsley Chen <kingsleyc6@nycstudents.net>
2026-01-21 18:17:37 +0000

    fzddzds

--- WDLesson3_6Practical/script.js
+++ WDLesson3_6Practical/script.js
@@ -1,5 +1,13 @@
 // Challenge 2:  Create the function for the Average program.  Refer to the resource link in the HTML for assistance.
-
+function g1(){
+    document.getElementById("g1").value = "grade 1"
+}
+function g2(){
+    document.getElementById("g2").value = "grade 2"
+}
+function g3(){
+    document.getElementById("g3").value = "grade 3"
+}
 
 // Challenge 4:  Create the function for the Slope program.  Refer to the resource link in the HTML for assistance.
 
